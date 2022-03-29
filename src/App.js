df7f3bf89520f936b3c9eb12678d2d7eef125210
santiago <truefush19@gmail.com>
2022-03-28 23:59:25 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/Navbar/NavBar';
 
 function App() {
+  const styles = {
+    color:'red',
+    background:'blue',
+  }
   return (
     <div className="App">
       <header className="App-header">
+      <NavBar />
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 style={styles}>
+          HOLA soy santiago
+        </h1>
+        <button>boton</button>
       </header>
     </div>
   );
---
+++ src/components/Navbar/NavBar.js
@@ -0,0 +1,16 @@
+import './NavBar.css'
+
+const NavBar = ()=>{
+    return(
+        <nav>
+            <ul>
+                <li><a href='#'>Home</a></li>
+                <li><a href='#'>Games</a></li>
+                <li><a href='#'>List</a></li>
+                <li><a href='#'>Status</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default NavBar
